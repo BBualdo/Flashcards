5e33bd2799e4a97fdc81b8cd663fbe132674fa54
Sebastian <mrseb10@gmail.com>
2024-04-14 13:53:18 +0200

    Creating Connection to SQL Server, Database and tables

---
+++ DatabaseLibrary/DbContext.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Data.SqlClient;
+using Spectre.Console;
+
+namespace DatabaseLibrary;
+
+public class DbContext
+{
+  public string ConnectionString { get; set; } = "Data Source=(localdb)\\C#AcademyProjects;Integrated Security=SSPI;TrustServerCertificate=True;Encrypt=false";
+
+  public DbContext()
+  {
+    AnsiConsole.Markup("[blue]Loading...[/]");
+    CreateDatabase();
+    CreateTables();
+  }
+
+  private void CreateDatabase()
+  {
+    using SqlConnection connection = new(ConnectionString);
+    connection.Open();
+
+    string sql = "IF NOT EXISTS(SELECT * FROM sys.databases WHERE name = 'flashcards') CREATE DATABASE flashcards";
+    using SqlCommand command = new(sql, connection);
+    command.ExecuteNonQuery();
+
+    ConnectionString = "Data Source=(localdb)\\C#AcademyProjects;Initial Catalog=flashcards;Integrated Security=SSPI;TrustServerCertificate=True;Encrypt=false";
+  }
+
+  private void CreateTables()
+  {
+    using SqlConnection connection = new(ConnectionString);
+    connection.Open();
+
+    string sql = @"IF NOT EXISTS(SELECT * FROM sys.tables WHERE schema_id=SCHEMA_ID('dbo') AND name='stacks') CREATE TABLE stacks(
+                  stack_id INT IDENTITY(1, 1) PRIMARY KEY,
+                  name VARCHAR(40) UNIQUE NOT NULL);
+
+                  IF NOT EXISTS(SELECT * FROM sys.tables WHERE schema_id=SCHEMA_ID('dbo') AND name='flashcards') CREATE TABLE flashcards(
+                  flashcard_id INT IDENTITY(1, 1) PRIMARY KEY,
+                  question VARCHAR(200) NOT NULL,
+                  answer VARCHAR(200) NOT NULL,
+                  stack_id INT REFERENCES stacks(stack_id));";
+    using SqlCommand command = new SqlCommand(sql, connection);
+    command.ExecuteNonQuery();
+  }
+}
--- Flashcards.BBualdo/Program.cs
+++ Flashcards.BBualdo/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DatabaseLibrary;
+using Spectre.Console;
+
+DbContext db = new();
+
+AnsiConsole.Clear();
+AnsiConsole.WriteLine("Press any key to continue");
+Console.ReadKey();
